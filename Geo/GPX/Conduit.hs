877c1a9a7fc902f8e805f7fd738bff558176d113
Peter Hartmann <phartmann@acm.org>
2026-02-12 17:00:45 +0100

    Successful tested with the > 1000 files I have from apps CrewNerd and myTracks.

--- Geo/GPX/Conduit.hs
+++ Geo/GPX/Conduit.hs
@@ -2,11 +2,10 @@
 {-| This is a partial parsing of the GPX 1.0 and 1.0 exchange types.
  -}
 module Geo.GPX.Conduit
-{-
         ( Track(..), GPX(..), Segment(..), Point(..)
         , readGPXFile
         , pt
-        ) -} where
+        ) where
 
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Resource
@@ -66,19 +65,17 @@ data Point           = Point
      , pntLon        :: Longitude
      , pntEle        :: Maybe Double -- ^ In meters
      , pntTime       :: Maybe UTCTime
-     -- , pntSpeed   :: Maybe Double -- ^ Non-standard.  Usually in meters/second.
-     , pntExts       :: Maybe Extension
+     , pntExts       :: Maybe Extensions
      } deriving (Eq, Ord, Show, Read)
 
-data Extension       = Extension 
-     { extName       :: Text
-     , extValue      :: Text
+data Extensions      = Extensions
+     { exts          :: [(Text,Text)]
      } deriving (Eq, Ord, Show, Read)
 
 {- | code changes are inspired by the tutorial 
 https://martin.hoppenheit.info/blog/2023/xml-stream-processing-with-haskell/
 -}
-pt :: Latitude -> Longitude -> Maybe Double -> Maybe UTCTime -> Maybe Extension -> Point
+pt :: Latitude -> Longitude -> Maybe Double -> Maybe UTCTime -> Maybe Extensions-> Point
 pt t g e m ext = Point t g e m ext
 
 readGPXFile :: FilePath -> IO (Maybe GPX)
@@ -93,58 +90,66 @@ readGPX = force "failed reading GPX file " $ parseGPX
 {- has worked except the xmlns attribute in the root element -}
 parseGPX :: MonadThrow m => ConduitT Event o m (Maybe GPX)
 parseGPX = 
-    tagIgnoreAttrs (defNs "gpx") (do
+    tagIgnoreAttrs (nsDef "gpx") (do
         metadata    <- parseMetadata
-        _           <- many_ $ ignoreTree (defNs "wpt") ignoreAttrs
+        _           <- many_ $ ignoreTree (nsDef "wpt") ignoreAttrs
         ts          <- many parseTrack
         return $ GPX (fromMaybe (Metadata "" "") metadata) ts)
 
 parseMetadata :: MonadThrow m => ConduitT Event o m (Maybe Metadata)
 parseMetadata = do
-    tagNoAttr (defNs "metadata")  (do
-        n           <- tagNoAttr (defNs "name") content
-        d           <- tagNoAttr (defNs "desc") content
-        _           <- ignoreTree (defNs "author") ignoreAttrs
-        _           <- ignoreTree (defNs "copyright") ignoreAttrs
-        _           <- ignoreTree (defNs "time") ignoreAttrs
+    tagNoAttr (nsDef "metadata")  (do
+        n           <- tagNoAttr (nsDef "name") content
+        d           <- tagNoAttr (nsDef "desc") content
+        _           <- ignoreTree (nsDef "author") ignoreAttrs
+        _           <- ignoreTree (nsDef "copyright") ignoreAttrs
+        _           <- ignoreTree (nsDef "time") ignoreAttrs
         return (Metadata (fromMaybe "egal" n) (fromMaybe "xegal" d)))
 
 parseTrack :: MonadThrow m => ConduitT Event o m (Maybe Track)
 parseTrack = do
-    tagNoAttr (defNs "trk") (do
-        n           <- tagNoAttr (defNs "name") content
-        -- d           <- tagNoAttr "desc" content
-        _           <- ignoreTree (nsExtensions "extensions") ignoreAttrs
+    tagNoAttr (nsDef "trk") (do
+        n           <- tagNoAttr (nsDef "name") content
+        d           <- tagNoAttr (nsDef "desc") content
+        _           <- ignoreTree (nsDef "extensions") ignoreAttrs
         segs        <- many parseSegment
         return (Track n Nothing segs))
 
 parseSegment :: MonadThrow m => ConduitT Event o m (Maybe Segment)
 parseSegment = do 
-    tagNoAttr (defNs "trkseg") (do
+    tagNoAttr (nsDef "trkseg") (do
         pnts        <- many parsePoint
         return (Segment pnts))
 
 parsePoint :: MonadThrow m => ConduitT Event o m (Maybe Point)
 parsePoint =
-    tag' (defNs "trkpt") parseAttributes $ \(lat, lon) -> (do
---        ele       <- tagNoAttr "ele" content
-        time        <- tagNoAttr (defNs "time") content
+    tag' (nsDef "trkpt") parseAttributes $ \(lat, lon) -> (do
+        ele         <- tagNoAttr (nsDef "ele") content
+        time        <- tagNoAttr (nsDef "time") content
         exts        <- parseExtension
         return $ Point { pntLon = parseDouble lon
                        , pntLat = parseDouble lat
                        , pntTime = parseUTC (fromMaybe "2026-02-10T13:09:01.237" time)
-                       , pntEle = Nothing --parseDouble <$> ele
+                       , pntEle = parseDouble <$> ele
                        , pntExts = exts 
                        })
   where
     parseAttributes = (,) <$> requireAttr "lat" <*> requireAttr "lon" <* ignoreAttrs
 
-parseExtension :: MonadThrow m => ConduitT Event o m (Maybe Extension)
+{- |
+Extensions are returned as pairs of (<element name>,<element content>).
+-}
+parseExtension :: MonadThrow m => ConduitT Event o m (Maybe Extensions)
 parseExtension =
-    tagNoAttr (defNs "extensions") (do 
-        hr          <- fromMaybe "" <$> tagNoAttr (nsExtensions "hr") content
-        ca          <- fromMaybe "" <$> tagNoAttr (nsExtensions "cadence") content
-        return $ Extension hr ca)
+    tagNoAttr (nsDef "extensions") (do 
+        exts        <- many parseExtensionEles
+        return $ Extensions exts)
+
+parseExtensionEles :: MonadThrow m => ConduitT Event o m (Maybe (Text,Text))
+parseExtensionEles =
+        tag anyName (\n -> (nameLocalName n) <$ ignoreAttrs) $ \ln -> do
+             c      <- content
+             return (ln,c)
 
 parseDouble :: Text -> Double
 parseDouble l = either (const 0) id (AT.parseOnly AT.double l)
@@ -162,9 +167,16 @@ parseUTC = either (const Nothing) id . AT.parseOnly (do
                         (unwords [yearMonthDay,hourMinSec,'.':fraction]))
         )
 
-defNs :: Text -> NameMatcher Name
-defNs n = matching (== Name n (Just "http://www.topografix.com/GPX/1/1") (Nothing))
+{- |
+This is required if there is a default namespace defined. Otherwises no nodes are matched.
+-}
+nsDef :: Text -> NameMatcher Name
+nsDef n = matching (== Name n (Just "http://www.topografix.com/GPX/1/1") (Nothing))
 
+{- |
+This is still a mystery: this is the extenion namespace of CrewNerd (a tracking app for rowers), 
+but nevertheless it works also for the app myTracks, which has a different extensions namespace.
+-}
 nsExtensions :: Text -> NameMatcher Name
 nsExtensions n = matching (== Name n (Just "http://www.cluetrust.com/XML/GPXDATA/1/0") (Just "gpxdata"))
 
--- app/Main.hs
+++ app/Main.hs
@@ -10,34 +10,88 @@ import System.IO
 import System.FilePath
 import Data.Text (toLower, pack)
 import Control.Exception (catch)
+import Control.Monad (filterM)
 import Data.Maybe (isJust)
+import System.Environment
+
+data STATUS = SUCCESS FilePath | FAILURE FilePath | EXCEPTION FilePath
+  deriving (Show, Eq)
 
 gpxDir :: FilePath
 gpxDir = "/home/peter/gpx"
 
+gpxFile:: FilePath -> FilePath
+gpxFile f = gpxDir </> f
+
 main  :: IO ()
 main = do 
-    -- f <- readFile "/home/peter/gpx/test.gpx"
-    -- print $ take 10 $ lines f
-    let f = gpxDir </> "2016-08-15 07_49_14.gpx"
-    gpx <- readGPXFile f
-    print (if isJust gpx then "success" else "failure")
+    args <- getArgs
+    if (length args == 0) 
+        then usage
+        else
+            if (head args == "m") 
+                then massTest
+                else do fe <- doesFileExist $ gpxFile (head args) 
+                        if fe
+                            then singleTest $ gpxFile $ head args
+                            else putStrLn ("File " ++ (head args) ++ " does not exist.")
+
+usage :: IO ()
+usage = putStrLn "main m | main <filename of gpx file in /home/peter/gpx>"
+
+singleTest :: FilePath -> IO ()
+singleTest gf = do 
+    gpx <- readGPXFile gf
+    print gpx
+    putStrLn ("Processing file '" ++ gf ++ "'->result=" ++ (if isJust gpx then "success" else "failure"))
     putStrLn "-------------------------"
-    -- runConduitRes $ parseFile def f .| renderBytes def .| stdoutC
 
 massTest :: IO ()
-massTest = do 
-    files <- listDirectory gpxDir
-    mapM_ (processGpx . (gpxDir </>)) $ filter ((".gpx"==) . toLower . pack . takeExtension) files 
+massTest = 
+    listDirectory gpxDir 
+        >>= filterM (return . (".gpx"==) . toLower . pack . takeExtension) 
+        >>= mapM (processGpx . (gpxDir </>))
+        >>= outputResult
 
-processGpx :: FilePath -> IO ()
+outputResult :: [ STATUS ] -> IO ()
+outputResult xs = do
+     putStrLn (concat [ "Total no. of processed files: "
+                      , show $ length xs 
+                      , "\n  No. of Successes: "
+                      , show $ length $ filter isSuccess xs 
+                      , "\n  No. of Failures: "
+                      , show $ length $ filter isFailure xs 
+                      , "\n  No. of Exceptions: "
+                      , show $ length $ filter isException xs 
+                      ]
+              )
+     putStrLn ">>>Failed files: "
+     mapM_ (putStrLn . show) $ filter isFailure xs
+     putStrLn ">>>files with exception: "
+     mapM_ (putStrLn . show) $ filter isException xs
+
+isSuccess :: STATUS -> Bool
+isSuccess (SUCCESS _) = True
+isSuccess _ = False
+
+isFailure :: STATUS -> Bool
+isFailure (FAILURE _) = True
+isFailure _ = False
+
+isException :: STATUS -> Bool
+isException (EXCEPTION _) = True
+isException _ = False
+
+processGpx :: FilePath -> IO (STATUS)
 processGpx f = do
     catch (do
         gpx <- readGPXFile f
         case gpx of
-            Just _ -> return ()
-            Nothing -> putStrLn ("GPX " ++ f ++ " failed to process.")
+            Just _ -> return $ SUCCESS f
+            Nothing -> return $ FAILURE f
         ) (handleXmlException f)
 
-handleXmlException :: FilePath -> XmlException -> IO ()
-handleXmlException f ex = putStrLn $ "Exception in " ++ f ++ ": " ++ (show ex)
+handleXmlException :: FilePath -> XmlException -> IO (STATUS)
+handleXmlException f ex = do
+    putStrLn $ "Exception in " ++ f ++ ": " ++ (show ex)
+    return $ EXCEPTION f
